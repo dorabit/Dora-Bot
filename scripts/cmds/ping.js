368da0adf0e6b9d2b09ddceaa82e92cbc1cc330a
dorabit <yogiwallet404@gmail.com>
2025-08-22 17:50:54 +0200

    Update ping.js

--- scripts/cmds/ping.js
+++ scripts/cmds/ping.js
@@ -1,36 +1,3 @@
-module.exports = {
-  config: {
-    name: "ping",
-    author: "GoatAI | Eldwin",
-    aliases: [],
-    category: "INFO",
-    role: 0,
-    shortDescription: {
-      en: "Ping command",
-      tl: "Ping command"
-    },
-    longDescription: {
-      en: "This command displays the bot's ping.",
-      tl: "Ibinibigay ng command na ito ang ping ng bot."
-    },
-    guide: {
-      en: "{p}ping",
-      tl: "{p}ping"
-    }
-  },
-  onStart: async function ({ event, message, threadsData, usersData, api }) {
-    const startTime = Date.now();
-    const msg = await message.reply("Pinging...");
-    const endTime = Date.now();
-
-    const ping = endTime - startTime;
-
-    message.reply(`Bot's ping is ${ping}ms.`);
-  },
-  onChat: async function ({ event, message, threadsData, usersData, api }) {
-    // You can leave this function empty for this command.
-  },
-  onReply: async function ({ event, message, threadsData, usersData, api, Reply }) {
-    // You can leave this function empty for this command.
-  }
-};
+🤖 اسم البوت: Dora Bot
+👨‍💻 المطور: حمودي سان 🇸🇩
+📡 سرعة البوت: XXms
