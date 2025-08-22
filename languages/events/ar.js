f990be389f38883fd5eebf034ff4f49de3088dcd
dorabit <yogiwallet404@gmail.com>
2025-08-22 18:16:55 +0200

    Update ar.js

--- languages/events/ar.js
+++ languages/events/ar.js
@@ -1,1 +1,16 @@
-
+module.exports = {
+  join: {
+    welcomeMessage: "🎉 أهلاً وسهلاً بـ {userName} في مجموعة {threadName}!\nنتمنى لك وقتًا ممتعًا معنا 💬"
+},
+  leave: {
+    leaveType1: "غادر بنفسه",
+    leaveType2: "تم طرده",
+    defaultLeaveMessage: "👋 {userName} {type} من المجموعة {threadName} في تمام الساعة {time} {session}"
+},
+  rename: {
+    renameMessage: "✏️ تم تغيير اسم المجموعة من \"{oldName}\" إلى \"{newName}\" بواسطة {authorName}"
+},
+  unsend: {
+    unsendMessage: "🚫 قام {userName} بحذف رسالة في المجموعة {threadName} في تمام الساعة {time}"
+}
+};
